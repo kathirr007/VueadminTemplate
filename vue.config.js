cb21a1fe3888cd53569a5eb17ec3d6f358c079fb
kathirr007 <kkumarasamy@edit-place.com>
2019-10-14 20:47:37 +0530

    Updated vue config for GH Pages

--- vue.config.js
+++ vue.config.js
@@ -8,6 +8,23 @@
 ==========================================================================================*/
 
 
+const webpack = require("webpack");
+
 module.exports = {
-  publicPath: '/'
+// The configure Web pack is the place in Vue CLI 3.0 to configure the parameters of the webpack plug-in. If you set it here, it will create or override the default configuration of the webpack.
+// The meaning of webpack ProvidePlugin is to create a global variable that can be used within each module of webpack. The configuration meaning here is to create three variables'$','jQuery','window. jQuery'pointing to jQuery dependency and'Popper' pointing to popper. JS dependency.
+publicPath : process.env.NODE_ENV === 'production'
+      ? '/vuesax-vuejs-admin-template/'
+      : '/',
+lintOnSave: false,
+configureWebpack: {
+        plugins: [
+            new webpack.ProvidePlugin({
+                $: 'jquery',
+                jQuery: 'jquery',
+                'window.jQuery': 'jquery',
+                Popper: ['popper.js', 'default']
+              })
+        ]
+      },
 }
